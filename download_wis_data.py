915a0a0d2e346d88dcb841ea9ecde857275831a9
collinjroland <45494890+collinjroland@users.noreply.github.com>
2024-04-30 14:54:00 -0500

    Add files via upload

--- download_wis_data.py
+++ download_wis_data.py
@@ -22,19 +22,30 @@ import random
 from time import sleep
 
 # %% Self-defined functions
+requests.adapters.DEFAULT_RETRIES = 5
+
+import requests
+
+from requests.adapters import HTTPAdapter, Retry
 
 def download_results(url, filename, overwrite=False):
-    response = requests.get(url)
+    s = requests.Session()
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+    response = s.get(url)
     path = pathlib.Path(os.path.join(os.getcwd(), filename))
     if overwrite or not path.exists():
         print('downloading {} to {}'.format(filename, path))
         open(filename, 'wb').write(response.content)
     else:
         print('{} already exists, skipping {}'.format(path, filename))
+    return response
             
 # %% Generate time bounds for request
 
-start_time = datetime(2021, 1, 1, 0, 0, 0)
+start_time = datetime(2007, 1, 1, 0, 0, 0)
 end_time = datetime(2021, 12, 1, 0, 0, 0)
 delta = timedelta(days = 30)
 dates = []
@@ -51,9 +62,10 @@ station = 'ST95259'
 format_str = 'csv'
 variables = 'waveHs,waveTp,waveMeanDirection,waveSpread,windDirection,windSpeed,waveTpPeak'
 variables = variables.replace(",", "%2C")
-for count in range(0, len(dates)):
-    while count < len(dates):
-        url_all = url_1 + region + '%2F' + station + '%2F' + station + '.nc4&time=' + str(dates[count]) + 'T00%3A00%3A00%2F' + str(dates[count + 1]) + 'T23%3A59%3A59&format=' + format_str + "&variables=" + variables + "&allYears=False"
-        filename = station + '_' + dates[count].isoformat().replace("-", "") + '.csv'
-        download_results(url_all, filename)
-        sleep(random.uniform(1,3))
+count = 0
+while count < len(dates):
+    url_all = url_1 + region + '%2F' + station + '%2F' + station + '.nc4&time=' + str(dates[count]) + 'T00%3A00%3A00%2F' + str(dates[count + 1]) + 'T23%3A59%3A59&format=' + format_str + "&variables=" + variables + "&allYears=False"
+    filename = station + '_' + dates[count].isoformat().replace("-", "") + '.csv'
+    response = download_results(url_all, filename)
+    sleep(random.uniform(5,10))
+    count += 1
